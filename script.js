8bef2757009bc0482e02275ed9745638b06daaf6
Filips Matvejevs <filissimo@gmail.com>
2023-04-09 20:35:12 +0300

    keyboard

--- script.js
+++ script.js
@@ -7,12 +7,17 @@ document.addEventListener("DOMContentLoaded", () => {
         grid_template_columns += "auto "
         for (let a = 0; a < 30; a++) {
             cell_id = `y${i}x${a}`
-            cell_data = {"y": i, "x": a}
+            cell_data = { "y": i, "x": a }
             cells_data.push(cell_data)
             grid.innerHTML += `
                 <div id="${cell_id}" class="cell"></div>
             `
         }
+    }
+    if (screen.width > screen.height) {
+        grid.classList.add("wide")
+    } else {
+        grid.classList.add("high")
     }
     grid.setAttribute("style", `grid-template-columns: ${grid_template_columns}`)
 
@@ -31,17 +36,19 @@ document.addEventListener("DOMContentLoaded", () => {
                 cursor_x_pos = cursor_id.indexOf("x")
                 cursor_y = +cursor_id.slice(1, cursor_x_pos)
                 cursor_x = +cursor_id.slice(cursor_x_pos + 1, cursor_id.length)
+
                 cell_id = cell_rendered.getAttribute("id")
                 x_pos = cell_id.indexOf("x")
                 cell_y = +cell_id.slice(1, x_pos)
                 cell_x = +cell_id.slice(x_pos + 1, cell_id.length)
                 x_difference = cursor_x - cell_x
+
                 if (x_difference < 0) {
-                    x_difference =  cell_x - cursor_x
+                    x_difference = cell_x - cursor_x
                 }
                 y_difference = cursor_y - cell_y
                 if (y_difference < 0) {
-                    y_difference =  cell_y - cursor_y
+                    y_difference = cell_y - cursor_y
                 }
                 if (x_difference > y_difference) {
                     if (cursor_x < cell_x && cursor_x < 30) {
@@ -68,7 +75,45 @@ document.addEventListener("DOMContentLoaded", () => {
                     }
                 }
             }
+            document.getElementById("console").innerHTML = `X:${cursorX}, Y:${cursorY}`
         }
     }
-
 })
+
+document.addEventListener('keydown', function (event) {
+    let cursor = document.querySelector(".cursor")
+    cursor_id = cursor.getAttribute("id")
+    cursor_x_pos = cursor_id.indexOf("x")
+    cursor_y = +cursor_id.slice(1, cursor_x_pos)
+    cursor_x = +cursor_id.slice(cursor_x_pos + 1, cursor_id.length)
+    if (event.keyCode == 37 && cursor_x > 0) {
+        let new_cursor = document.getElementById(`y${cursor_y}x${cursor_x - 1}`)
+        cursor.classList.remove("cursor")
+        new_cursor.classList.add("cursor")
+        // alert('Left was pressed');
+    }
+    else if (event.keyCode == 39  && cursor_x < 29) {
+        let new_cursor = document.getElementById(`y${cursor_y}x${cursor_x + 1}`)
+        cursor.classList.remove("cursor")
+        new_cursor.classList.add("cursor")
+        // alert('Right was pressed');
+    }
+    else if (event.keyCode == 38  && cursor_y > 0) {
+        let new_cursor = document.getElementById(`y${cursor_y - 1}x${cursor_x}`)
+        cursor.classList.remove("cursor")
+        new_cursor.classList.add("cursor")
+        // alert('Up was pressed');
+    }
+    else if (event.keyCode == 40 && cursor_y < 29) {
+        let new_cursor = document.getElementById(`y${cursor_y + 1}x${cursor_x}`)
+        cursor.classList.remove("cursor")
+        new_cursor.classList.add("cursor")
+        // alert('Down was pressed');
+    }
+})
+
+document.addEventListener('click', printMousePos, true);
+function printMousePos(e) {
+    cursorX = e.pageX;
+    cursorY = e.pageY;
+}
