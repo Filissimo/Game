43cb0ad8f9faf78f648c7751877e3a444cc623ce
Filips Matvejevs <filissimo@gmail.com>
2023-04-14 01:06:36 +0300

    btn font

--- script.js
+++ script.js
@@ -84,7 +84,7 @@ document.addEventListener("DOMContentLoaded", () => {
 
     let start_moving = ''
 
-    let speed = 0.09
+    let speed = 0.1
 
     joystick.onmouseover = () => {
         clearInterval(start_moving)
