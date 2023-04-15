29cb9acabae8b0d07d4ff14f486702184da41d68
Filips Matvejevs <filissimo@gmail.com>
2023-04-15 12:40:59 +0300

    scary_page

---
+++ script2.js
@@ -0,0 +1,30 @@
+document.addEventListener("DOMContentLoaded", () => {
+    var elem = document.documentElement;
+    function openFullscreen() {
+        if (elem.requestFullscreen) {
+            elem.requestFullscreen();
+        } else if (elem.webkitRequestFullscreen) { /* Safari */
+            elem.webkitRequestFullscreen();
+        } else if (elem.msRequestFullscreen) { /* IE11 */
+            elem.msRequestFullscreen();
+        }
+    }
+    function closeFullscreen() {
+        if (document.exitFullscreen) {
+            document.exitFullscreen();
+        } else if (document.webkitExitFullscreen) { /* Safari */
+            document.webkitExitFullscreen();
+        } else if (document.msExitFullscreen) { /* IE11 */
+            document.msExitFullscreen();
+        }
+    }
+
+    btn = document.querySelector("button")
+    bsod = document.querySelector("img")
+
+    btn.onclick = () => {
+        btn.classList.add("invisible")
+        bsod.classList.remove("invisible")
+        openFullscreen()
+    }
+})
