c53e72836bc0ca434843d919a560411ce555d0bf
Filips Matvejevs <filissimo@gmail.com>
2023-04-17 01:22:55 +0300

    enemy nerfed

--- script.js
+++ script.js
@@ -120,12 +120,12 @@ document.addEventListener("DOMContentLoaded", () => {
     function reset_enemy_stats() {
         enemy_limit = 20
         enemy_speed = 1
-        enemy_speed_upgr = 0.02
+        enemy_speed_upgr = 0.01
         spawn_interval = 1500
         enemy_health_max = 1
         enemy_health_max_upgr = 0.1
         enemy_damage = 1
-        enemy_damage_upgr = 0.1
+        enemy_damage_upgr = 0.03
     }
     reset_enemy_stats()
     function enemy_upgr() {
